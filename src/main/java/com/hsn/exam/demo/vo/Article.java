726321aff30f9024a43db000ddc701c094bb77bb
sinaehwang <godvv29756318@gmail.com>
2022-09-26 16:14:58 +0900

    article doAdd구현완료/ doAdd한 게시물 리스트 보여지도록 구현

--- src/main/java/com/hsn/exam/demo/vo/Article.java
+++ src/main/java/com/hsn/exam/demo/vo/Article.java
@@ -2,20 +2,15 @@ package com.hsn.exam.demo.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-@Data //getter를 일괄적으로 사용것과 동일
-@AllArgsConstructor// 생성자를 대신해서사용
-@NoArgsConstructor //생성자를 args없이도 만들수 있게해줌
 
 
-	public class Article {
-
-	@Getter //private을 보여주기 위해 사용됨
-	 private int id;
-	@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+	private int id;
 	private String title;
-	
 	private String body;
-	 
+
 }
