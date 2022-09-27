69cc850b9f3a6ca27578c6d8c01e89392667e16f
sinaehwang <godvv29756318@gmail.com>
2022-09-27 16:16:00 +0900

    ArticleService 도입완료/ ArticleRepository 도입후 Service 메소드기능 이전완료

--- src/main/java/com/hsn/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/hsn/exam/demo/controller/UsrArticleController.java
@@ -13,79 +13,63 @@ import com.hsn.exam.demo.vo.Article;
 @Controller
 public class UsrArticleController {
 
-	//인스턴스변수
-	@Autowired //new ArticleService 객체를 만든거나 마찬가지(서비스와 컨트롤러를 연결해준다)
-	
-	private ArticleService articleService;
-	
+	// 인스턴스 변수
+		@Autowired
+		private ArticleService articleService;
 
-	//액션메서드
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
-		
-		Article article = articleService.writeArticle(title, body);
+		// 액션메서드
+		@RequestMapping("/usr/article/doAdd")
+		@ResponseBody
+		public Article doAdd(String title, String body) {
+			Article article = articleService.writeArticle(title, body);
 
-				return article;
-
-	}
-	
-	
-	@RequestMapping("/usr/article/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		
-	      return articles;
-	   }
-	
-	@RequestMapping("/usr/article/doDelete")
-	@ResponseBody
-	public String doDelete(int id) {
-		Article article = articleService.getArticle(id);
-
-		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다";
+			return article;
 		}
 
-		deleteArticle(id);
-
-		return id + "번 게시물을 삭제했습니다";
-	}
-	
-	@RequestMapping("/usr/article/doModify")
-	@ResponseBody
-	public Object doModify(int id,String title, String body) {
-		Article article = articleService.getArticle(id);
-
-		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다";
+		@RequestMapping("/usr/article/getArticles")
+		@ResponseBody
+		public List<Article> getArticles() {
+			return articleService.articles();
 		}
 
-		modifyArticle(id,title,body);
+		@RequestMapping("/usr/article/doDelete")
+		@ResponseBody
+		public String doDelete(int id) {
+			Article article = articleService.getArticle(id);
 
-		return article;
-	}
-	
-	@RequestMapping("/usr/article/getArticle")
-	@ResponseBody
-	public Object getArticle(int id,String title, String body) {
-		Article article = getArticle(id);
+			if (article == null) {
+				return id + "번 게시물은 존재하지 않습니다";
+			}
 
-		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다";
+			articleService.deleteArticle(id);
+
+			return id + "번 게시물을 삭제했습니다";
 		}
 
-		return article;
+		@RequestMapping("/usr/article/doModify")
+		@ResponseBody
+		public Object doModify(int id, String title, String body) {
+			Article article = articleService.getArticle(id);
+
+			if (article == null) {
+				return id + "번 게시물은 존재하지 않습니다";
+			}
+
+			articleService.modifyArticle(id, title, body);
+
+			return article;
+		}
+
+		@RequestMapping("/usr/article/getArticle")
+		@ResponseBody
+		public Object getArticleAction(int id) {
+			Article article = articleService.getArticle(id);
+
+			if (article == null) {
+				return id + "번 게시물은 존재하지 않습니다";
+			}
+
+			return article;
+		}
+
 	}
-
-
-
-	
-
-
-
-	
-	
-}
-
-
--- src/main/java/com/hsn/exam/demo/service/ArticleService.java
+++ src/main/java/com/hsn/exam/demo/service/ArticleService.java
@@ -1,73 +1,51 @@
 package com.hsn.exam.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.hsn.exam.demo.repository.ArticleRepository;
 import com.hsn.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
 	
-	private int lastArticleId;
-	private List<Article> articles;
-	
-	public ArticleService() {
-		lastArticleId = 0;
-		articles = new ArrayList<>();
+	private ArticleRepository articleRepository;
+
+	// 생성자
+	public ArticleService(ArticleRepository articleRepository) {
 		
-		makeTest();
-		
+		this.articleRepository = articleRepository;
+
+		articleRepository.makeTestData();
 	}
-	
-	
-	//서비스메서드
-	private void makeTest() {
 
-		for(int i=1; i<=10; i++) {
-			int id = lastArticleId+1;
-			String title = "제목"+i;
-			String body = "내용"+i;
-			
-			writeArticle(title, body);
-		}
+	// 서비스메서드
 
-	   }
-	
-	public Article writeArticle(String title, String body) {
-		int id = lastArticleId+1;
-
-		Article article = new Article(id, title, body);
-		
-		articles.add(article);
-		lastArticleId=id;
-		
-		return article;
-	}
-	
 	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-
-		return null;
-	}
-	
-	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-
-		articles.remove(article);
-	}
-	
-	public void modifyArticle(int id,String title, String body) {
 		
-		Article article = getArticle(id);
-		article.setTitle(title);
-		article.setBody(body);
-				
+		return articleRepository.getArticle(id);
+	}
+		
+
+	public Article writeArticle(String title, String body) {
+
+		return articleRepository.writeArticle(title,body);
 	}
 
+	public void deleteArticle(int id) {
+		
+		articleRepository.deleteArticle(id);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		
+		articleRepository.modifyArticle(id, title, body);
+		
+	}
+
+	public List<Article> articles() {
+		
+		return articleRepository.articles();
+	}
 }
