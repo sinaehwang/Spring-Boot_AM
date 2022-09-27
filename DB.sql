fc28a1cd450b5fd7724379ada8fe0515f30aa132
sinaehwang <godvv29756318@gmail.com>
2022-09-27 17:38:17 +0900

    DB스키마 추가

---
+++ DB.sql
@@ -0,0 +1,36 @@
+# DB 생성
+DROP DATABASE IF EXISTS SB_AM;
+DROP TABLE SB_AM;
+CREATE DATABASE SB_AM;
+USE SB_AM;
+
+# 게시글 테이블 생성
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+# 게시글 테스트데이터 생성
+
+INSERT INTO article 
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article 
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+INSERT INTO article 
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3';
+
+SELECT *FROM article
