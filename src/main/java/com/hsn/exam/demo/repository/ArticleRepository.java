e2d886025064de9933b4a08cee3ffe822274163d
sinaehwang <godvv29756318@gmail.com>
2022-09-28 16:30:24 +0900

    INSERT 게시글 추가 구현완료, SQL을 xml로 이전

--- src/main/java/com/hsn/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/hsn/exam/demo/repository/ArticleRepository.java
@@ -2,38 +2,23 @@ package com.hsn.exam.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.hsn.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
 
-	@Select("SELECT * FROM article WHERE id = #{id}") 
 	public Article getArticle(int id);
-	//SELECT * FROM article WHERE id=?
 	
-	@Insert("INSERT INTO article SET regDate=NOW(),updateDate = NOW(), title=#{title},`body`=#{body};")
 	public void writeArticle(String title, String body);
-	//INSERT INTO article SET regDate=NOW(),updateDate = NOW(), title=?,`body`=?;
 	
-	@Delete("DELETE FROM article WHERE id=#{id}")
 	public void deleteArticle(int id);
-	//DELETE FROM article WHERE id=?
 	
-	@Update("UPDATE article SET title=#{title}, `body`=#{body} WHERE id=#{id}")
 	public void modifyArticle(int id, String title, String body);
-	//UPDATE article SET title=?, `body`=? WHERE id=?
 	
-	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> articles();
-	//SELECT * FROM article ORDER BY id DESC;
 	
-	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastArticleId();
 	
 	
--- src/main/java/com/hsn/exam/demo/service/ArticleService.java
+++ src/main/java/com/hsn/exam/demo/service/ArticleService.java
@@ -28,11 +28,11 @@ public class ArticleService {
 	}
 		
 
-	public Article writeArticle(String title, String body) {
+	public int writeArticle(String title, String body) {
 
 		 articleRepository.writeArticle(title,body);
 		 
-		 int id = articleRepository.getLastArticleId();
+		return articleRepository.getLastArticleId();
 	}
 
 	public void deleteArticle(int id) {
