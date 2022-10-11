2eaa077ad60b02b71dbd143222ff63f275c7e3da
sinaehwang <godvv29756318@gmail.com>
2022-10-11 17:49:29 +0900

    BeforeActionIntercepter 구현

---
+++ src/main/java/com/hsn/exam/demo/intercepter/BeforeActionIntercepter.java
@@ -0,0 +1,17 @@
+package com.hsn.exam.demo.intercepter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class BeforeActionIntercepter implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		System.out.println("실행됨?");
+		return HandlerInterceptor.super.preHandle(req, resp, handler);
+	}
+}
